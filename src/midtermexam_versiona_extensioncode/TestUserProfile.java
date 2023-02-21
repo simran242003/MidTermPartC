62764fbb7fb333b32ad230f215e974cb7cc919ba
Simran <simran33@sheridancollege.ca>
2023-02-21 10:10:00 -0600

    It is a commit for mid term

---
+++ src/midtermexam_versiona_extensioncode/TestUserProfile.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author Simran
+ */
+import java.util.Scanner;
+public class TestUserProfile {
+    
+    public static void main(String args[]){
+        System.out.println("Enter your name");
+                Scanner input=new Scanner(System.in);
+                String name=input.next();
+                
+                System.out.println("Enter your favourite genre number");
+                int n=input.nextInt();
+                
+                System.out.println("Your profile is created");
+                
+                
+    }
+    
+}
